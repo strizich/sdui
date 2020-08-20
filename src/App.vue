95c7c62a2f3879d8f984352d0cfd7714afcfb968
Jason Strizich <strizich@me.com>
2020-08-20 11:54:05 -0500

    update vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app-container">
+  <div id="app">
     <router-view/>
   </div>
 </template>
--- src/library/components/SdPopover/index.js
+++ src/library/components/SdPopover/index.js
@@ -1,3 +1,5 @@
 import SdPopover from './SdPopover'
+import SdTooltip from './SdTooltip'
 
+export { SdTooltip }
 export default SdPopover
