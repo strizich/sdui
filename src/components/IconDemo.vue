f10ac16513f7fd3d29c94c14726f581e4f9c82bc
Jason Strizich <strizich@me.com>
2020-09-02 18:19:22 -0500

    Added simple search to icons

--- src/components/IconDemo.vue
+++ src/components/IconDemo.vue
@@ -2,25 +2,54 @@
   <section>
     <p class="sd--text__footnote">Powered by: Material Icons Font</p>
     <h1>Icons</h1>
+    <sd-fieldset>
+      <sd-field>
+        <sd-label>Icon Search</sd-label>
+        <input type="search" v-model="iconSearch"/>
+        <sd-error>This component is a WIP</sd-error>
+      </sd-field>
+    </sd-fieldset>
+    <select v-model="resultCount">
+      <option value="25">25</option>
+      <option value="100">100</option>
+      <option value="200">200</option>
+      <option value="300">300</option>
+      <option value="400">400</option>
+    </select>
     <div class="demo__icons">
-      <div class="demo__icon" v-for="name in matIcons" :key="name">
+      <div class="demo__icon" v-for="(name, index) in totalFilteredResults" :key="name + index">
         <sd-icon size="lg" :name="name" />
         <small>{{name}}</small>
       </div>
     </div>
+  {{simpleSearchArray}}
   </section>
 </template>
 
 <script>
 import MAT_ICONS from '@/assets/MatIconList'
-import { SdIcon } from '@/library'
-export default {
+import { defineComponent, computed, ref } from 'vue'
+import { SdFieldset, SdIcon, SdLabel, SdField, SdError } from '@/library'
+
+export default defineComponent({
   setup () {
-    const matIcons = MAT_ICONS
-    return { matIcons }
+    const matIcons = MAT_ICONS.sort()
+    const iconSearch = ref('')
+    const resultCount = ref(25)
+    const searchResult = computed(() => {
+      return matIcons.filter((icon) => {
+        return icon.toLowerCase().includes(iconSearch.value.toLowerCase())
+      })
+    })
+
+    const totalFilteredResults = computed(() => {
+      return searchResult.value.slice(0, resultCount.value)
+    })
+
+    return { iconSearch, matIcons, searchResult, resultCount, totalFilteredResults }
   },
-  components: { SdIcon }
-}
+  components: { SdIcon, SdFieldset, SdField, SdLabel, SdError }
+})
 </script>
 
 <style lang="scss" scoped>
@@ -39,11 +68,17 @@ export default {
     align-items: center;
     justify-content: center;
     margin: 8px 0;
-    width: 200px;
     height: 20%;
     white-space: wrap;
+    width: calc(100% / 6);
+    @include breakpoint-down('md'){
+       width: calc(100% / 4);
+    }
     @include breakpoint-down('sm'){
-      width: 33%;
+      width: calc(100% / 3);
+    }
+    @include breakpoint-down('xs'){
+      width: calc(100% / 2);
     }
   }
 }
