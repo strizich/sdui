97e08371e1a59f1a572baab5c0215f84b3f8d76a
Jason Strizich <strizich@me.com>
2020-10-01 11:02:18 -0500

    update service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -21,6 +21,23 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated () {
       console.log('New content is available; please refresh.')
+      // Caching with service workers: https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker#removing_outdated_caches
+      self.addEventListener('activate', (event) => {
+        event.waitUntil(
+          caches.keys().then((cacheNames) => {
+            return Promise.all(
+              cacheNames.filter((cacheName) => {
+                return true
+                // Return true if you want to remove this cache,
+                // but remember that caches are shared across
+                // the whole origin
+              }).map((cacheName) => {
+                return caches.delete(cacheName)
+              })
+            )
+          })
+        )
+      })
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
